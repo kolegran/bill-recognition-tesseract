ff0582f295548f729cf8cf6f93c920695e819b5f
kolegran <kolegran@gmail.com>
2019-04-17 18:32:15 +0300

    Implemented bill recognition from an image in base64

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import os
 import pytesseract
+import base64
 
 from PIL import Image
 from flask import Flask
+from flask import json
+from flask import request
+from io import BytesIO
 
 app = Flask(__name__)
 
-@app.route('/')
-def get_bill_content():
-	return '<pre>' + pytesseract.image_to_string(Image.open('bill.jpg'), lang='ukr') + '</pre>'
+@app.route('/images', methods = ['POST'])
+def get_image_content():
+  json_data = json.loads(json.dumps(request.json))
+  
+  image = base64.b64decode(json_data["image_base_64"])
+  image_file = BytesIO(image)
+  image_file.seek(0)
+
+  return pytesseract.image_to_string(Image.open(image_file), lang='ukr')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
