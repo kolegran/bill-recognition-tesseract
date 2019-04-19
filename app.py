3223c2568791d98e18235147b84e69a9f0010a64
kolegran <kolegran@gmail.com>
2019-04-19 17:35:39 +0300

    Renamed some names

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ from io import BytesIO
 
 app = Flask(__name__)
 
-@app.route('/images', methods = ['POST'])
+@app.route('/image-content', methods = ['POST'])
 def get_image_content():
   json_data = json.loads(json.dumps(request.json))
   
-  image = base64.b64decode(json_data["image_base_64"])
+  image = base64.b64decode(json_data["imageInBase64"])
   image_file = BytesIO(image)
   image_file.seek(0)
 
   return pytesseract.image_to_string(Image.open(image_file), lang='ukr')
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+  app.run(debug = True, host = '0.0.0.0')
